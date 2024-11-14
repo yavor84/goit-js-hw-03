2f8f3449da56bb8af3385b2ec36ed0364b609871
Dmytro Yavor <pojardim@gmail.com>
2024-11-14 23:25:05 +0200

    add tasks

---
+++ js/task-1.js
@@ -0,0 +1,9 @@
+const slugify = function (title) {
+  const slug = title.toLowerCase().split(' ');
+  return slug.join('-');
+};
+
+console.log(slugify('Arrays for beginners')); // "arrays-for-beginners"
+console.log(slugify('English for developer')); // "english-for-developer"
+console.log(slugify('Ten secrets of JavaScript')); // "ten-secrets-of-javascript"
+console.log(slugify('How to become a JUNIOR developer in TWO WEEKS')); // "how-to-become-a-junior-developer-in-two-weeks"
---
+++ js/task-3.js
@@ -0,0 +1,15 @@
+const filterArray = function (numbers, value) {
+  const numbersArray = [];
+  for (const item of numbers) {
+    if (item > value) {
+      numbersArray.push(item);
+    }
+  }
+  return numbersArray;
+};
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
